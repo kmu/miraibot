0a76b1d5a25d3a54719fcf3f0559c32ada57dbec
Aslan168 <kan.tomo.1688@gmail.com>
2023-02-24 11:38:20 +0900

    modified jobtime

--- bot.py
+++ bot.py
@@ -255,19 +255,19 @@ def pretty_lab_update():
                     if(oldest_jobtime > jobtime):
                         oldest_jobtime = jobtime
 
-                    total_jobtime = (nowtime - oldest_jobtime).total_seconds()
-                    if total_jobtime< 4*60: # under 1h
-                        time_emoji = f":{int(total_jobtime/60)}m:"
-                    elif total_jobtime< 60*60: # under 1h
-                        time_emoji = f":{int(total_jobtime/60/15)*15}m:"
-                    elif total_jobtime < 4*60*60: # under 4h
-                        time_emoji = f":{int(total_jobtime/60/60)}h:"
-                    elif total_jobtime < 24*60*60: # under 1d
-                        time_emoji = f":{int(total_jobtime/60/60/4)*4}h:"
-                    elif total_jobtime < 14*24*60*60: # under 2week
-                        time_emoji = f":{int(total_jobtime/60/60/24)}d:"
-                    else:
-                        time_emoji = f":over14d:"
+                total_jobtime = (nowtime - oldest_jobtime).total_seconds()
+                if total_jobtime< 4*60: # under 1h
+                    time_emoji = f":{int(total_jobtime/60)}m:"
+                elif total_jobtime< 60*60: # under 1h
+                    time_emoji = f":{int(total_jobtime/60/15)*15}m:"
+                elif total_jobtime < 4*60*60: # under 4h
+                    time_emoji = f":{int(total_jobtime/60/60)}h:"
+                elif total_jobtime < 24*60*60: # under 1d
+                    time_emoji = f":{int(total_jobtime/60/60/4)*4}h:"
+                elif total_jobtime < 14*24*60*60: # under 2week
+                    time_emoji = f":{int(total_jobtime/60/60/24)}d:"
+                else:
+                    time_emoji = f":over14d:"
 
             jobtime_d[q_group] += [time_emoji]
 
